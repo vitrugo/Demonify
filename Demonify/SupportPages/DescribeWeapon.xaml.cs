9b38f082eabe8c6f52a24cb4be6fec94640c5c35
João Pedro Barbosa de Almeida <60893114+JoaoBAlmeida@users.noreply.github.com>
2020-05-31 18:19:34 -0300

    Add files via upload

---
+++ Demonify/SupportPages/DescribeWeapon.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Demonify.Classes;
+
+namespace Demonify.SupportPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DescribeWeapon : ContentPage
+    {
+        public delegate bool PopDelegate(bool delete);
+        public event PopDelegate Finished;
+        private Items item;
+        public DescribeWeapon(Items items)
+        {
+            InitializeComponent();
+            item = items;
+            BindingContext = item;
+            if (item is HeavyWeapon) LblStat.Text = "STR";
+            else if (item is MediumWeapon) LblStat.Text = "DEX";
+            else LblStat.Text = "WIS";
+            LblEquip.Text = item.equipped.ToString();
+        }
+
+        private void BtnEquip(object sender, EventArgs e)
+        {
+            item.equipped = !item.equipped;
+            LblEquip.Text = item.equipped.ToString();
+            if(item.equipped) Inventory.slot1 = item;
+            else Inventory.slot1 = null;
+            if(item.equipped) DisplayAlert("Sucesso", "Item equipado com sucesso", "OK");
+            else DisplayAlert("Sucesso", "Item desequipado com sucesso", "OK");
+        }
+
+        private async void BtnDelete(object sender, EventArgs e)
+        {
+            bool ansr = await DisplayAlert("CONFIRMAÇÃO", "Certeza de que deseja deletar este item de seu inventário? " +
+                "(Caso equipado, será retirado de sua mão primeiro)", "SIM", "NÂO");
+            if (ansr)
+            {
+                if (item.equipped)
+                {
+                    item.equipped = false;
+                    Inventory.slot1 = null;
+                }
+                Inventory.Inv.Remove(item);
+                Finished(true);
+                await Navigation.PopAsync();
+            }
+            else return;
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            Finished(false);
+            return base.OnBackButtonPressed();
+        }
+    }
+}
---
+++ Demonify/SupportPages/DescriptionArmor.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Demonify.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demonify.SupportPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DescriptionArmor : ContentPage
+    {
+        public delegate bool PopDelegate(bool bought);
+        public event PopDelegate Finished;
+        private int XP, Cost;
+        public DescriptionArmor(Items items, int xp)
+        {
+            InitializeComponent();
+            BindingContext = items;
+            XP = xp;
+            Cost = items.Cost;
+            if (items is HeavyArmor)
+            {
+                LblDex.Text = LblWis.Text = "0";
+            }
+            else if (items is MediumArmor)
+            {
+                LblStr.Text = LblWis.Text = "0";
+            }
+            else LblStr.Text = LblDex.Text = "0";
+        }
+
+        private async void BtnBuy(object sender, EventArgs e)
+        {
+            bool ansr = await DisplayAlert("Buy Action", "Certeza de que deseja realizar esta compra?", "SIM", "NÃO");
+            if (ansr)
+            {
+                if(XP < Cost)
+                {
+                    await DisplayAlert("ERROR", "XP insuficiente.\nNecessários " + Cost + "XP para realizar a compra.", "OK");
+                    return;
+                }
+                await Navigation.PopAsync();
+                Finished(true);
+            }
+            else return;
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            Finished(false);
+            return base.OnBackButtonPressed();
+        }
+    }
+}
